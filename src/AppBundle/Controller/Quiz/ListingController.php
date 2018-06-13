2f5422f6121fa30c81892b7523c0e786b631951d
MasterKafei <MasterKafei59@gmail.com>
2018-06-13 12:39:40 +0200

    Fix pagination quiz search

--- src/AppBundle/Controller/Quiz/ListingController.php
+++ src/AppBundle/Controller/Quiz/ListingController.php
@@ -69,18 +69,19 @@ class ListingController extends Controller
         $search = $request->query->get('search');
 
         if ('' == $pageNumber || $pageNumber < 0) {
-           $pageNumber = 0;
+           $pageNumber = 1;
         }
 
         /** @var Paginator $paginator */
-        $paginator = $this->getDoctrine()->getRepository(Quiz::class)->findAllRecent($pageNumber, $search);
+        $paginator = $this->getDoctrine()->getRepository(Quiz::class)->findAllRecent($pageNumber - 1, $search);
         $quizzes = $paginator->getIterator()->getArrayCopy();
         $total = $paginator->count();
 
         return $this->render('@Page/Quiz/Listing/list.html.twig', array(
+            'search' => $search,
             'quizzes' => $quizzes,
             'total' => $total,
-            'current_page' => $pageNumber + 1,
+            'current_page' => $pageNumber,
             'result_by_page' => 8,
         ));
     }
