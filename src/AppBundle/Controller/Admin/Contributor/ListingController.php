e117d8f0c61b101a18c6a9a28ffc3c94dea12c1b
MasterKafei <MasterKafei59@gmail.com>
2018-08-08 18:57:17 +0200

    Fix sb-admin-2 dependencie

---
+++ src/AppBundle/Controller/Admin/Contributor/ListingController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AppBundle\Controller\Admin\Contributor;
+
+
+class ListingController
+{
+
+}
---
+++ src/AppBundle/Form/Model/Question/ResponseModel.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AppBundle\Form\Model\Question;
+
+
+class ResponseModel
+{
+
+}
